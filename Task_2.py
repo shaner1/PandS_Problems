5e7f07fdc520206ae1f9ab806fdd8a978e4edbaa
Shane Rylands <Shane@Shanes-MacBook-Pro.local>
2020-02-16 20:38:49 +0000

    renaming files

--- Task_2.py
+++ Task_2.py
@@ -1,9 +1,15 @@
-#This program will calculate your BMI
+#Task Week 2
+# This program will calculate your BMI
 
+#Will take inputs of height and weight from the user
 height = float(input("What is your height in cm : "))
 weight = float(input("What is your weight in kg: "))
 
+
+#need to square the height
 h2 = (height/100)**2
+
+#bmi = weight in kg / height squared in cm
 bmi = int(weight//h2)
 
 print ("Your BMI is"), bmi
