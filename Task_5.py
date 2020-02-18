2546f10b4bafb260b9cb218d129d7f5fc277e396
Shane Rylands <Shane@Shanes-MacBook-Pro.local>
2020-02-18 22:36:01 +0000

    This is the Task for week 5 that tells you whether it is a week day or the weekend

---
+++ Task_5.py
@@ -0,0 +1,18 @@
+#This program will let you know whether it is a week day or not 
+
+#Import the library Datetime
+import datetime
+
+message1= ("Today is a Weekday")
+message2= ("Today is the Weekend :)")
+
+now = datetime.datetime.now()
+
+#Define the variable day
+day = now.weekday()
+
+#If day is less than 4, than it will be a weekday.
+if day < 4:
+    print(message1)
+else:
+    print(message2)
